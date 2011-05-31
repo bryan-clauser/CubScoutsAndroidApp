c69d52f58a08c548361cb1308ae5aa7c671e5142
Bryan Clauser <bmclauser@gmail.com>
2011-05-31 17:20:44 -0500

    Added Eclipse Project

---
+++ src/com/bsa/android/Pack30AndroidApp.java
@@ -0,0 +1,13 @@
+package com.bsa.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Pack30AndroidApp extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
